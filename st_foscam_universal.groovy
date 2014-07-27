bc3beaa840d95f97139cf1bf017f3ed6235262e8
skp19 <skorpeon25@gmail.com>
2014-07-26 17:14:38 -0700

    Version 1

--- st_foscam_universal.groovy
+++ st_foscam_universal.groovy
@@ -43,9 +43,9 @@ metadata {
         }
 
 		standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
-			state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF"//, nextState:"taking"
-			//state "taking", label:'Taking', action: "", icon: "st.camera.take-photo", backgroundColor: "#53a7c0"
-			state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF"//, nextState:"taking"
+			state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
+			state "taking", label:'Taking', action: "", icon: "st.camera.take-photo", backgroundColor: "#53a7c0"
+			state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
 		}
 
         standardTile("alarmStatus", "device.alarmStatus", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
@@ -62,9 +62,9 @@ metadata {
         
         standardTile("ledAuto", "device.ledStatus", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
           state "auto", label: "auto", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#53A7C0"
-          state "off", label: "off", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
-          state "on", label: "on", action: "ledAuto", icon: "st.Lighting.light11", backgroundColor: "#FFFFFF"
-          state "manual", label: "manual", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
+          state "off", label: "auto", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
+          state "on", label: "auto", action: "ledAuto", icon: "st.Lighting.light11", backgroundColor: "#FFFFFF"
+          state "manual", label: "auto", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
         }
 
         standardTile("ledOn", "device.ledStatus", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
@@ -95,9 +95,9 @@ metadata {
 }
 
 def take() {
-	log.debug("Take a photo")
+	log.debug("Taking Photo")
 	sendEvent(name: "hubactionMode", value: "s3");
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
 		hubGet("cmd=snapPicture2")
     }
     else {
@@ -118,7 +118,7 @@ def toggleAlarm() {
 def alarmOn() {
 	log.debug "Enabling Alarm"
     sendEvent(name: "alarmStatus", value: "on");
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
 		hubGet("cmd=setMotionDetectConfig&isEnable=1")
     }
     else {
@@ -129,7 +129,7 @@ def alarmOn() {
 def alarmOff() {
 	log.debug "Disabling Alarm"
     sendEvent(name: "alarmStatus", value: "off");
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
 		hubGet("cmd=setMotionDetectConfig&isEnable=0")
     }
     else {
@@ -157,7 +157,7 @@ def toggleLED() {
 def ledOn() {
     log.debug("LED changed to: on")
     sendEvent(name: "ledStatus", value: "on");
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
 	    delayBetween([hubGet("cmd=setInfraLedConfig&mode=1"), hubGet("cmd=openInfraLed")])
     }
     else {
@@ -168,7 +168,7 @@ def ledOn() {
 def ledOff() {
     log.debug("LED changed to: off")
     sendEvent(name: "ledStatus", value: "off");
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
     	delayBetween([hubGet("cmd=setInfraLedConfig&mode=1"), hubGet("cmd=closeInfraLed")])
     }
     else {
@@ -179,7 +179,7 @@ def ledOff() {
 def ledAuto() {
     log.debug("LED changed to: auto")
     sendEvent(name: "ledStatus", value: "auto");
-	if(hdcamera == true) {
+	if(hdcamera == "true") {
 		hubGet("cmd=setInfraLedConfig&mode=0")
     }
     else {
@@ -191,11 +191,16 @@ def poll() {
 
 	sendEvent(name: "hubactionMode", value: "local");
     //Poll Motion Alarm Status and IR LED Mode
-	delayBetween([hubGet("cmd=getMotionDetectConfig"), hubGet("cmd=getInfraLedConfig")])
+    if(hdcamera == "true") {
+		delayBetween([hubGet("cmd=getMotionDetectConfig"), hubGet("cmd=getInfraLedConfig")])
+    }
+    else {
+    	hubGet("/get_params.cgi?")
+    }
 }
 
 private getLogin() {
-	if(hdcamera == true) {
+	if(hdcamera == "true") {
     	return "&usr=${username}&pwd=${password}"
     }
     else {
@@ -204,15 +209,15 @@ private getLogin() {
 }
 
 private hubGet(def apiCommand) {
-	//SET NETWORK DEVICE ID HERE
+	//Setting Network Device Id
     def iphex = convertIPtoHex(ip)
     def porthex = convertPortToHex(port)
     device.deviceNetworkId = "$iphex:$porthex"
     log.debug "Device Network Id set to ${iphex}:${porthex}"
 
-	log.debug("Executing get api to " + getHostAddress())
+	log.debug("Executing hubaction on " + getHostAddress())
     def uri = ""
-    if(hdcamera == true) {
+    if(hdcamera == "true") {
     	uri = "/cgi-bin/CGIProxy.fcgi?" + apiCommand + getLogin()
 	}
     else {
@@ -238,9 +243,6 @@ def parse(String description) {
     def map = [:]
     def retResult = []
     def descMap = parseDescriptionAsMap(description)
-    log.debug "parse returns $descMap"
-    log.debug "Bucket: " + descMap["bucket"]
-    log.debug "Key: " + descMap["key"]
         
     //Image
 	if (descMap["bucket"] && descMap["key"]) {
@@ -250,13 +252,11 @@ def parse(String description) {
 	//Status Polling
     else if (descMap["headers"] && descMap["body"]) {
         def body = new String(descMap["body"].decodeBase64())
-        if(hdcamera == true) {
+        if(hdcamera == "true") {
             def langs = new XmlSlurper().parseText(body)
 
             def motionAlarm = "$langs.isEnable"
-            log.debug "MOTION ALARM: $motionAlarm"
             def ledMode = "$langs.mode"
-            log.debug "LED MODE: $ledMode"
 
             //Get Motion Alarm Status
             if(motionAlarm == "0") {
@@ -271,13 +271,24 @@ def parse(String description) {
             //Get IR LED Mode
             if(ledMode == "0") {
                 log.info("Polled: LED Mode Auto")
-                sendEvent(name: "ledStatus", value: "auto");
+                sendEvent(name: "ledStatus", value: "auto")
             }
             else if(ledMode == "1") {
                 log.info("Polled: LED Mode Manual")
-                sendEvent(name: "ledStatus", value: "manual");
+                sendEvent(name: "ledStatus", value: "manual")
             }
     	}
+        else {
+        	if(body.find("alarm_motion_armed=0")) {
+				log.info("Polled: Alarm Off")
+                sendEvent(name: "alarmStatus", value: "off")
+            }
+        	else if(body.find("alarm_motion_armed=1")) {
+				log.info("Polled: Alarm On")
+                sendEvent(name: "alarmStatus", value: "on")
+            }
+            //The API does not provide a way to poll for LED status on 8xxx series at the moment
+        }
 	}
 }
 
@@ -322,13 +333,11 @@ private getHostAddress() {
 
 private String convertIPtoHex(ipAddress) { 
     String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
-    log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
     return hex
 
 }
 
 private String convertPortToHex(port) {
 	String hexport = port.toString().format( '%04x', port.toInteger() )
-    log.debug hexport
     return hexport
 }
