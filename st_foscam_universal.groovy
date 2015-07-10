e4d3649b444e2980e91fd84e785c679b7caef897
skp19 <skp19@users.noreply.github.com>
2015-07-09 18:19:51 -0700

    Update st_foscam_universal.groovy

--- st_foscam_universal.groovy
+++ st_foscam_universal.groovy
@@ -5,6 +5,16 @@
  *
  *  modified 2015-06-04 :  thrash99er  - changed bool comparsions from string to bool  i.e.  "true" to true
  *
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ * 
  */
 metadata {
 	definition (name: "Foscam Universal Device", namespace: "skp19", author: "skp19") {
